85e8272ab2a131a662da8d6eba24171133725d92
Jerry <jerrywu98@hotmail.com>
2023-10-18 15:02:40 -0600

    step 3 - convert temperature from fahrenheit to celsius

--- temp.py
+++ temp.py
@@ -2,7 +2,15 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
 
-celsius = 25
-fahrenheit = celsius_to_fahrenheit(celsius)
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) / 9/5
 
-print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+# celsius = 25
+# fahrenheit = celsius_to_fahrenheit(celsius)
+
+fahrenheit = 82
+celsius = fahrenheit_to_celsius(fahrenheit)
+
+# print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+print(f"{fahrenheit} Fahrenheit to Celsius is {celsius}")
